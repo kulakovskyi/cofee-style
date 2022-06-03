5ba95abe111d5c7f28d3134cb8771410d2f36f00
Andriy <81190009+kulakovskyi@users.noreply.github.com>
2022-06-03 09:50:51 +0300

    Delete CofeeStyle project directory

--- CofeeStyle project/script.js
+++
@@ -1,8 +0,0 @@
-const iconMenu = document.querySelector(".menu__icon");
-const menuBody = document.querySelector(".header__menu");
-
-iconMenu.addEventListener("click", function (e) {
-  document.body.classList.toggle("_lock");
-  iconMenu.classList.toggle("_active");
-  menuBody.classList.toggle("_active");
-});
